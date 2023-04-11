a9a90b3c37821cf4b341e95f0c942ff11e8a6c8e
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-04-11 20:41:08 +0200

    change attributes of spans

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3158,7 +3158,10 @@ class View {
         const curElements = Array.from(this._parentElement.querySelectorAll("*"));
         newElements.forEach((newEl, i)=>{
             const curEl = curElements[i];
-            console.log(curEl, newEl.isEqualNode(curEl));
+            // updates changed text
+            if (!newEl.isEqualNode(curEl) && newEl.firstChild?.nodeValue.trim() !== "") curEl.textContent = newEl.textContent;
+            // updates changed attributes
+            if (!newEl.isEqualNode(curEl)) Array.from(newEl.attributes).forEach((attr)=>curEl.setAttribute(attr.name, attr.value));
         });
     }
     _clear() {
