fa33d923841b3a2f79ed2391c3331015c7cfe08f
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-04-19 15:40:09 +0200

    spacing bugfix

--- src/js/model.js
+++ src/js/model.js
@@ -121,7 +121,7 @@ export const uploadRecipe = async function (newRecipe) {
     const ingredients = Object.entries(newRecipe)
       .filter((entry) => entry[0].startsWith('ingredient') && entry[1] !== '')
       .map((ing) => {
-        const ingArr = ing[1].replaceAll(' ', '').split(',');
+        const ingArr = ing[1].split(',').map((ing) => ing.trim());
         if (ingArr.length !== 3)
           throw new Error(
             'Wrong ingredient format! Please use correct format.'
